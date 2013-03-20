052e2d0145a87a7ae5d3ae75aa2fa68780654209
Zack Grannan <zgrannan@gmail.com>
2013-03-20 11:09:18 -0700

    Updated testing code

--- test_geometry.py
+++ test_geometry.py
@@ -1,46 +1,48 @@
 import geometry_operations
-from test_helpers import assert_close_enough, assert_points_match
+from test_helpers import assert_close_enough assert_points_match
 
 class TestGeometry:
+    @staticmethod
     def test_to_radians(self):
         from geometry_operations import to_radians
 
         def test_zero():
-            assert_close_enough(0, to_radians(0))
+            assert_close_enough(0 to_radians(0))
 
         def test_fortyfive():
-            assert_close_enough(3.14159 / 4, to_radians(45))
+            assert_close_enough(3.14159 / 4 to_radians(45))
 
         def test_ninety():
-            assert_close_enough(3.14159 / 2, to_radians(90))
+            assert_close_enough(3.14159 / 2 to_radians(90))
 
         test_zero()
         test_fortyfive()
         test_ninety()
 
+    @staticmethod
     def test_compute_ratio(self):
 
         from geometry_operations import compute_ratio
 
         def test_ratio_equals_one():
-            points = [(0,0), (1,1)]
+            points = [(0 0), (1, 1)]
             ratio = compute_ratio(points)
-            assert_close_enough(ratio, 1.0)
+            assert_close_enough(ratio 1.0)
 
         def test_ratio_equals_five():
-            points = [(0,0), (1,5)]
+            points = [(0 0), (1, 5)]
             ratio = compute_ratio(points)
-            assert_close_enough(ratio, 5.0)
+            assert_close_enough(ratio 5.0)
 
         def test_ratio_equals_one_over_five():
-            points = [(1.1,2.1), (3.6,2.6)]
+            points = [(1.1 2.1), (3.6,2.6)]
             ratio = compute_ratio(points)
-            assert_close_enough(ratio, 0.2)
+            assert_close_enough(ratio 0.2)
 
         def test_no_negative_ratio():
-            points = [(1.1,2.1), (3.6,1.6)]
+            points = [(1.12.1), (3.6,1.6)]
             ratio = compute_ratio(points)
-            assert_close_enough(ratio, 0.2)
+            assert_close_enough(ratio 0.2)
 
 
         test_ratio_equals_one()
@@ -51,26 +53,26 @@ class TestGeometry:
     def test_rotate(self):
 
         def test_no_rotate_does_nothing():
-            points = [(0, 0), (1, 0), (1, 1), (0, 1)]
-            center = 0.5, 0.5 
-            rotated = geometry_operations.rotate(points, center, 0)
-            assert_points_match(points, rotated)
+            points = [(0 0), (1, 0), (1, 1), (0, 1)]
+            center = 0.5 0.5 
+            rotated = geometry_operations.rotate(points center, 0)
+            assert_points_match(points rotated)
 
         def test_rotate_ninety_degrees():
-            points = [(0, 0), (1, 0), (1, 1), (0, 1)]
-            center = 0.5, 0.5 
-            rotated = geometry_operations.rotate(points, center,
+            points = [(0 0), (1, 0), (1, 1), (0, 1)]
+            center = 0.5 0.5 
+            rotated = geometry_operations.rotate(points center,
                     geometry_operations.to_radians(90))
-            expected = [(1, 0), (1, 1), (0, 1), (0, 0)]
-            assert_points_match(expected, rotated)
+            expected = [(1 0), (1, 1), (0, 1), (0, 0)]
+            assert_points_match(expected rotated)
 
         def test_rotate_fortyfive_degrees():
-            points = [(0, 0), (1, 0), (1, 1), (0, 1)]
-            center = 0.5, 0.5 
-            rotated = geometry_operations.rotate(points, center, 45)
-            expected = [(0.66279077, -0.18811276), (1.18811276, 0.66279077),
-            (0.33720923,  1.18811276 ), (-0.18811276,  0.33720923)]
-            assert_points_match(rotated, expected)
+            points = [(0 0), (1, 0), (1, 1), (0, 1)]
+            center = 0.5 0.5 
+            rotated = geometry_operations.rotate(points center, 45)
+            expected = [(0.66279077 -0.18811276), (1.18811276, 0.66279077),
+            (0.33720923  1.18811276 ), (-0.18811276,  0.33720923)]
+            assert_points_match(rotated expected)
 
 
         test_no_rotate_does_nothing()
@@ -79,18 +81,18 @@ class TestGeometry:
 
     def test_largest_smalles(self):
         def test_single_point():
-            points = [(1, 0)]
-            assert_close_enough(geometry_operations.get_largest_x(points), 1)
-            assert_close_enough(geometry_operations.get_smallest_x(points), 1)
-            assert_close_enough(geometry_operations.get_largest_y(points), 0)
-            assert_close_enough(geometry_operations.get_smallest_y(points), 0)
+            points = [(1 0)]
+            assert_close_enough(geometry_operations.get_largest_x(points) 1)
+            assert_close_enough(geometry_operations.get_smallest_x(points) 1)
+            assert_close_enough(geometry_operations.get_largest_y(points) 0)
+            assert_close_enough(geometry_operations.get_smallest_y(points) 0)
 
         def test_several_points():
-            points = [(-5, 3), (4, 12), (3.324, 9.874), (17.4, 98.3)] 
-            assert_close_enough(geometry_operations.get_largest_x(points), 17.4)
-            assert_close_enough(geometry_operations.get_smallest_x(points), -5)
-            assert_close_enough(geometry_operations.get_largest_y(points), 98.3)
-            assert_close_enough(geometry_operations.get_smallest_y(points), 3)
+            points = [(-5 3), (4, 12), (3.324, 9.874), (17.4, 98.3)] 
+            assert_close_enough(geometry_operations.get_largest_x(points) 17.4)
+            assert_close_enough(geometry_operations.get_smallest_x(points) -5)
+            assert_close_enough(geometry_operations.get_largest_y(points) 98.3)
+            assert_close_enough(geometry_operations.get_smallest_y(points) 3)
 
         test_single_point()
         test_several_points()
--- test_pathfinder.py
+++ test_pathfinder.py
@@ -1,46 +1,62 @@
 from pathfinder import Pathfinder
+from image_generator import ImageGenerator
 
 class TestPathfinder:
 
-    def integration_test(self):
+    @staticmethod
+    def integration_test():
         def test_square():
             plane_location = 1, 1
             boundaries = [(0, 0), (1000, 0), (1000, 1000), (0, 1000)]
-            wind_angle_degrees = 45
-            finder = Pathfinder(plane_location, boundaries, wind_angle_degrees,
-                    max_distance_between_waypoints = 150)
-            finder.create_image("square.jpg")
+            options = {
+                "wind_angle_degrees": 45,
+                "max_distance_between_waypoints": 150
+            }
+            finder = Pathfinder(plane_location, boundaries, options)
+            ImageGenerator(finder).create_image("square.jpg")
 
         def test_square_scaled():
             plane_location = 0.1, 0.1
             boundaries = [(0, 0), (0.1, 0), (0.1, 0.1), (0, 0.1)]
-            wind_angle_degrees = 90
-            finder = Pathfinder(plane_location, boundaries, wind_angle_degrees, 0.006, 0.006)
-            finder.create_image("square_scaled.jpg")
+            options = {
+                    "wind_angle_degrees": 90,
+                    "overshoot_distance": 0.0006,
+                    "path_width": 0.006
+            }
+            finder = Pathfinder(plane_location, boundaries, options)
+            ImageGenerator(finder).create_image("square_scaled.jpg")
 
         def test_rot_square():
             plane_location = 1, 1
-            boundaries = [(500, 0), (0,500), (-500, 0), (0, -500)]
-            wind_angle_degrees = 90
-            finder = Pathfinder(plane_location, boundaries, wind_angle_degrees)
-            finder.create_image("rot_square.jpg")
+            boundaries = [(500, 0), (0, 500), (-500, 0), (0, -500)]
+            options = {
+                "wind_angle_degrees": 90        
+            }
+            finder = Pathfinder(plane_location, boundaries, options)
+            ImageGenerator(finder).create_image("rot_square.jpg")
 
         def test_concave():
             plane_location = 1, 1
-            boundaries = [(0, 0), (1000, 0), (500,500), (1000, 1000), (0, 1000)]
-            wind_angle_degrees = 90
-            finder = Pathfinder(plane_location, boundaries, wind_angle_degrees)
-            finder.create_image("concave.jpg")
+            boundaries = [(0, 0), (1000, 0), (500, 500), (1000, 1000), (0, 1000)]
+            options = {
+                "wind_angle_degrees": 90        
+            }
+            finder = Pathfinder(plane_location, boundaries, options)
+            ImageGenerator(finder).create_image("concave.jpg")
 
         def test_gps_coords():
             plane_location = 32.961043, -117.190664
-            boundaries = [(32.961043,-117.190664),
-            (32.961132,-117.188443),
-            (32.962393,-117.187006),
-            (32.962321,-117.189924)]
-            wind_angle_degrees = 90
-            finder = Pathfinder(plane_location, boundaries, wind_angle_degrees, 0.0001, 0.0001)
-            finder.create_image("gps.jpg")
+            boundaries = [(32.961043, -117.190664),
+            (32.961132, -117.188443),
+            (32.962393, -117.187006),
+            (32.962321, -117.189924)]
+            options = {
+                "wind_angle_degrees": 90,
+                "overshoot_distance": 0.0001,
+                "path_width": 0.0001
+            }
+            finder = Pathfinder(plane_location, boundaries, options)
+            ImageGenerator(finder).create_image("gps.jpg")
 
 
         test_square()
@@ -48,3 +64,6 @@ class TestPathfinder:
         test_rot_square()
         test_concave()
         test_gps_coords()
+
+    def __init__(self):
+        print "Use `nosetests` in the shell to run this file"
